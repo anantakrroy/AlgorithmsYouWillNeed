a48bfce1a8afdbc4533c461c34de5306e389dcf2
anantakrroy <kr.innovate@gmail.com>
2024-10-25 15:33:19 +0530

    Bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,14 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
     // 1st iteration bubbles the largest to the end of arr ; repeat this till only 1 element remains to be iterated over
-    for(let j = arr.length; j <= 1; --j) {
-        for(let i = 0 ; i < arr.length - i ; ++i) {
+    for(let j = arr.length; j > 1; --j) {
+        for(let i = 0 ; i < j - 1 ; ++i) {
             if(arr[i] > arr[i+1]) {
-                let temp = arr[i+1]
-                arr[i+1]= arr[i]
-                arr[i] = temp
-                console.log('array bubbling >> ', arr);
+                let temp = arr[i]   
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
             } 
-            
         }
     }
 }
